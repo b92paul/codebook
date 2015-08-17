c77624a28913210b2de85c80e88ffb23809a404c
david942j <b00901096@ntu.edu.tw>
2015-08-17 14:19:47 +0800

    Update default.cpp

--- default.cpp
+++ default.cpp
@@ -2,7 +2,9 @@
 #define openfile(s) freopen(s".in","r",stdin);freopen(s".out","w",stdout)
 #define mpr std::make_pair
 #define lg(x) (31-__builtin_clz(x))
+#define lgll(x) (63-__builtin_clzll(x))
 #define __count __builtin_popcount
+#define __countll __builtin_popcountll
 #define X first
 #define Y second
 #define mst(x) memset(x,0,sizeof(x))
@@ -12,6 +14,7 @@
 #define FOR1(i,n) for(int i=1;i<=n;i++)
 #define FORit(it,c) for(__typeof(c.begin()) it=c.begin();it!=c.end();++it)
 #define pb push_back
+#define RI(x) scanf("%d",&x)
 #define RID(x) int x;RI(x)
 using namespace std;
 typedef long long LL;
@@ -21,13 +24,8 @@ typedef std::pair<int,int> PII;
 template<class T>inline void maz(T &a,T b){if(a<b)a=b;}
 template<class T>inline void miz(T &a,T b){if(a>b)a=b;}
 template<class T>inline T abs(T a){return a>0?a:-a;}
-void RI() {}
-template<typename... T>
-void RI( int& head, T&... tail ) {
-    scanf("%d",&head);
-    RI(tail...);
-}
-const int N=100100;
+const int N=100100, INF=1e9;
+const LD EPS=1e-7;
 
 int main(){
 	//openfile("");
