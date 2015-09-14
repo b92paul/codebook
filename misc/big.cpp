431b011f4c25991d4934023e443389601051a408
david942j <david942j@gmail.com>
2015-09-14 20:47:43 +0800

    只留struct

--- misc/big.cpp
+++ misc/big.cpp
@@ -1,44 +1,4 @@
 //by david942j
-#include <cstdio>
-#include <cstring>
-#include <set>
-#include <map>
-#include <ctime>
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include <cmath>
-#include <deque>
-#include <cassert>
-#include <queue>
-#include <stack>
-#include <cstdlib>
-#define openfile(s) freopen(s".in","r",stdin);freopen(s".out","w",stdout)
-#define mpr std::make_pair
-#define lg(x) (31-__builtin_clz(x))
-#define lgll(x) (63-__builtin_clzll(x))
-#define __count __builtin_popcount
-#define __countll __builtin_popcountll
-#define X first
-#define Y second
-#define mst(x) memset(x,0,sizeof(x))
-#define mst1(x) memset(x,-1,sizeof(x))
-#define ALL(c) (c).begin(),(c).end()
-#define FOR(i,n) for(int i=0;i<n;i++)
-#define FOR1(i,n) for(int i=1;i<=n;i++)
-#define FORit(it,c) for(__typeof(c.begin()) it=c.begin();it!=c.end();++it)
-#define pb push_back
-#define RI(x) scanf("%d",&x)
-#define RID(x) int x;RI(x)
-using namespace std;
-typedef long long LL;
-typedef double LD;
-typedef vector<int> VI;
-typedef std::pair<int,int> PII;
-template<class T>inline void maz(T &a,T b){if(a<b)a=b;}
-template<class T>inline void miz(T &a,T b){if(a>b)a=b;}
-template<class T>inline T abs(T a){return a>0?a:-a;}
-const int N=10010,INF=1e9;
 struct Big{
     #define mod 10000000LL // 7
     //#define MN 1000 // max digit = 1000*7 
@@ -147,5 +107,3 @@ struct Big{
         puts("");
     }
 };
-int main() {
-}
