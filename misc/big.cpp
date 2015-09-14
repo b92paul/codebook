65c2e84c1cd0244635cbd95391dea5a3be226d6b
david942j <david942j@gmail.com>
2015-09-14 20:44:10 +0800

    除法

--- misc/big.cpp
+++ misc/big.cpp
@@ -88,6 +88,7 @@ struct Big{
         tmp.norm();
         return tmp;
     }
+    // for minus
     bool operator<(const Big &A)const {
         if(s!=A.s)return s < A.s;
         bool f= s==-1;
@@ -109,6 +110,35 @@ struct Big{
         FOR(i,h()) if(tmp[i]<0)tmp[i+1]--,tmp[i]+=mod;
         tmp.norm();
         return tmp;
+    }
+    // for division
+    void shift(int h) {
+        p.resize(p.size() + h);
+        for(int i=p.size()-1; i>=h; i--)
+            p[i]=p[i-h];
+        for(int i=h-1;i>=0;i--)p[i]=0;
+    }
+    Big operator/(const Big &A)const {
+        if(h() < A.h()) return Big();
+        if(A[A.h()-1] == 0) throw "divided by 0";
+        Big c = *this, ans;
+        c.s=1;
+        for(int i=h()-A.h();i>=0;i--) {
+            Big tmp=A; tmp.shift(i); tmp.s=1;
+            
+            int low=0,up=mod-1;
+            while(low<=up) {
+                int mid=low+up>>1;
+                if(!(c<tmp*mid)) low=mid+1;
+                else up=mid-1;
+            }
+            c = c-tmp*up;
+            ans.p.pb(up);
+        }
+        reverse(ALL(ans.p));
+        ans.s=s*A.s;
+        ans.norm();
+        return ans;
     }
     void print() {
         if(s<0)putchar('-');
